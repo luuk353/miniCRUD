859c39976c4678669e1470202e6b80b636c96788
luuk353 <94046799+luuk353@users.noreply.github.com>
2022-03-17 10:47:50 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href = "css/main.css">
+    <link rel= " " href = "js/main.js">
+</head>
+<body>
+     
+
+<p>
+    <?php
+    $breedte = 10;
+    $lengte = 11;
+    $hoogte = 5;
+    $oppervlakte = ($breedte * $lengte);
+    echo "<p> de breedte is {$breedte}</p>";
+    echo "het oppervlakte is ",($oppervlakte), "m2";
+    ?>
+</p>
+
+</body>
+</html>
