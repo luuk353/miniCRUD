618103846c79a84c84413c17577b0508a426a6ab
luuk353 <1198506@student.roc-nijmegen.nl>
2022-03-24 11:35:18 +0100

    yooo

---
+++ winkelmand.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+    <header>
+
+        <a href="index.php">index page</a>
+        <a href="bestel.php">bestellijst</a>
+        <a href="login.php">log in</a>
+        <div class = "image" onclick="location.href='winkelmand.php';" style="cursor: pointer;">
+        </div>
+        <?php
+// include("connect.php");
+?>
+
+    </header>
+    <main>
+        <div class="winkelmand">
+            <h1>Uw winkelmand</h1>
+            <p> YOOOOOO </p>
+        </div>
+
+    </main>
+    <footer>
+
+    </footer>
+</body>
+
+</html>
